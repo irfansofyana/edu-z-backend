9ab43d30d89739302d3d368f2e43c57ab22f6245
irfansofyana <irfansofyana0305@gmail.com>
2020-12-15 01:06:37 +0700

    Create model feedback

---
+++ models/feedbacks.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const feedbackSchema = new Schema({
+    rating: {
+        type: Number,
+        enum: [1, 2, 3, 4, 5],
+        required: true
+    },
+    comment: {
+        type: String,
+    },  
+    givenBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Students'
+    }
+}, {
+    timestamps: true
+});
+
+const feedbacks = mongoose.model('Feedbacks', feedbackSchema);
+
+module.exports = feedbacks;
