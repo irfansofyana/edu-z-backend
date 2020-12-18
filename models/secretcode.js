385348a20d3b67d7c071a630e6cd9ccb2fb8c27e
irfansofyana <irfansofyana0305@gmail.com>
2020-12-18 22:57:35 +0700

    Add secretcode

---
+++ models/secretcode.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const secretCodeSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    code: {
+        type: String,
+        required: true
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now(),
+        expires: 600
+    }
+});
+
+const SecretCode = mongoose.model('SecretCode', secretCodeSchema);
+
+module.exports = SecretCode;
