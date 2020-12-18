8aedf0a7243657f3c32d8157378ab3933ce87b4d
irfansofyana <irfansofyana0305@gmail.com>
2020-12-18 22:43:45 +0700

    edit sendEmail function

--- helper/email.js
+++ helper/email.js
@@ -10,12 +10,11 @@ const smtp = nodemailer.createTransport(smtpTransport({
     }
 }));
 
-const sendEmail = (receiver) => {
-    const link="https://google.com";
+const sendEmail = (receiver, link) => {
     const mailOptions = {
         to: receiver,
         subject: "Please confirm your account",
-        html: "Hello,<br> Please Click on the link to verify your email.<br><a href="+link+">Click here to verify</a>"
+        html: "Hello,<br> Please Click on the link to verify your account in Edu-Z application.<br><a href="+link+">Click here to verify</a>"
     }
     smtp.sendMail(mailOptions, (err, res) => {
         if (err) {
